016f8c1e99fc953fdb5f3760e12693acd395f15a
vanny <vdevelopper@gmail.com>
2022-07-27 17:07:33 +0200

    comparaison des id de body avec celle de idEvenement

--- controller/scanController.js
+++ controller/scanController.js
@@ -9,16 +9,33 @@ const create = async (req, res) => {
               if (!validation.isValidate()) {
                      return res.status(422).json({ errors: validation.getErrors() });
                }
-              const { insertId } = await scanModel.createOne(
-                     req.userId,
-                     PATH,
-                     EVENEMENT_ID
-              );
-              const user = (await scanModel.findById("ID_USERS_SCAN", insertId))[0];
-              res.status(200).json({
-                     succes: true,
-                     message:"L'enregistrement est faites avec succes"
-              });
+
+              const  idEvenement = (await scanModel.findEvenementId(EVENEMENT_ID))[0];
+
+              //console.log(idEvenement.EVENEMENT_ID)
+              if(!idEvenement == '') {
+                     var evenementId= idEvenement.EVENEMENT_ID
+                     const { insertId } = await scanModel.createOne(
+                            req.userId,
+                            PATH,
+                            evenementId
+                     );
+                     res.status(200).json({
+                            succes: true,
+                            message:"L'enregistrement est faites avec succes"
+                     });
+
+                     //const user = (await scanModel.findById("ID_USERS_SCAN", insertId))[0];
+                     console.log(req.userId +" "+ PATH +" "+evenementId)
+
+              }
+              else{
+                     res.status(401).json({
+                            succes:false,
+                            message:"L'enregistrement a ete echoue"
+                     })
+              } 
+             
        }
        catch(error){
               console.log(error)
--- model/scanModel.js
+++ model/scanModel.js
@@ -3,18 +3,29 @@ const {query} = require("../functions/db")
 const findById = async (id) => {
        try {
          return query("SELECT * FROM users_scan WHERE ID_USERS_SCAN  = ?", [
-           id,
+           id
          ]);
        } catch (error) {
          throw error;
        }
 }
 
-const createOne = (userId, PATH, EVENEMENT_ID) =>{
+const createOne = (userId, PATH, evenementId) =>{
        try{
+              
               return query(
                      "INSERT INTO users_scan(USER_ID,PATH,EVENEMENT_ID) VALUES(?,?,?)",
-                     [userId, PATH, EVENEMENT_ID]
+                     [userId, PATH, evenementId]
+              );
+       }
+       catch(error){
+              throw error
+       }
+}
+
+const findEvenementId =async (id) => {
+       try{
+              return query(`SELECT EVENEMENT_ID FROM evenement_evenement WHERE  md5(EVENEMENT_ID) = ?`,[id]
               );
        }
        catch(error){
@@ -25,5 +36,6 @@ const createOne = (userId, PATH, EVENEMENT_ID) =>{
 
 module.exports={
        findById,
-       createOne
+       createOne,
+       findEvenementId
 }
